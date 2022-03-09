eb735e6d02a66cb4e89128ba3f372d98d05633c7
Mohammad Hosen Saikat <83907907+m0h4mm4d-h053n@users.noreply.github.com>
2022-03-09 21:47:38 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function countdown() {
+  const timeSpan = new Date("April 1,2022 00:00:00").getTime() - new Date().getTime();
+  
+  // 1day = 86400000 milisec, 1hour = 3600000 milisec, 1minute = 60000 milisec
+  const inputDay = Math.floor(timeSpan / 86400000);
+  const inputHour = Math.floor((timeSpan % 86400000) / 3600000);
+  const inputMin = Math.floor((timeSpan % 3600000) / 60000);
+  const inputSec = Math.floor((timeSpan % 60000) / 1000);
+
+  document.querySelector(".day").innerText = inputDay;
+  document.querySelector(".hour").innerText = inputHour;
+  document.querySelector(".minute").innerText = inputMin;
+  document.querySelector(".second").innerText = inputSec;
+}
+
+setInterval(countdown, 1000);
